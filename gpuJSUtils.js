6eee3e0f27089f58178953c8eaa8a1321f3b66fa
Bubba Skrimp <brewster.joshua1@gmail.com>
2020-07-30 20:10:59 -0800

    updated with example fft.js code right in the WIP stuff for reference

--- gpuJSUtils.js
+++ gpuJSUtils.js
@@ -69,6 +69,38 @@ class gpuUtils {
       //BitReverseIndex(index,n)
       //BitReverseComplexArray(array)
 
+      /*
+        function BitReverseIndex(index, n) {
+          let bitreversed_index = 0;
+
+          while (n > 1) {
+            bitreversed_index <<= 1;
+            bitreversed_index += index & 1;
+            index >>= 1;
+            n >>= 1;
+          }
+          return bitreversed_index;
+        }
+
+        function BitReverseComplexArray(array) {
+          const n = array.length;
+          const flips = new Set();
+
+          for(let i = 0; i < n; i++) {
+            const r_i = BitReverseIndex(i, n);
+
+            if (flips.has(i)) continue;
+
+            [array.real[i], array.real[r_i]] = [array.real[r_i], array.real[i]];
+            [array.imag[i], array.imag[r_i]] = [array.imag[r_i], array.imag[i]];
+
+            flips.add(r_i);
+          }
+
+          return array;
+        }
+      */
+
       this.getRecursive = this.gpu.createKernel(function (input,p){ //Not sure about this yet.
         var recursive_result = new Array(2);
         recursive_result[0] = input[0][this.thread.x*p + this.thread.y]; //this.thread.y does not work on a 1D output. Need to solve this
@@ -80,25 +112,83 @@ class gpuUtils {
       .setDynamicArguments(true); //setDynamic output allows setOutput to be called for different sized arrays
 
 
-      //UNFINISHED
+      //UNFINISHED. This needs to be broken up properly in separate kernels then combined. Reference fft.js
       this.FFT_Recursive = this.gpu.createKernel(function(input, len, inverse){ //This needs to be done with a combined kernel so that getRecursive can be called
+        
+        /* OG CODE
+          const n = input.length;
+
+          if (n === 1) {
+            return input;
+          }
+
+          const output = new ComplexArray(n, input.ArrayType);
+
+          // Use the lowest odd factor, so we are able to use FFT_2_Iterative in the
+          // recursive transforms optimally.
+          const p = LowestOddFactor(n);
+          const m = n / p;
+          const normalisation = 1 / Math.sqrt(p);
+          let recursive_result = new ComplexArray(m, input.ArrayType);
+
+        */
+        
         if(len === 1){
           return input;
         }
 
         const p = lof(len);
-        const m = n/p;
+        const m = ;en/p;
         const normalize = 1 / Math.sqrt(p);
         var recursive_result = [];
 
+        /* OG CODE
+          // Loops go like O(n Σ p_i), where p_i are the prime factors of n.
+          // for a power of a prime, p, this reduces to O(n p log_p n)
+          for(let j = 0; j < p; j++) { //this.thread.y
+            for(let i = 0; i < m; i++) { //this.thread.x
+              recursive_result.real[i] = input.real[i * p + j];
+              recursive_result.imag[i] = input.imag[i * p + j];
+            }
+            // Don't go deeper unless necessary to save allocs.
+            if (m > 1) {
+              recursive_result = fft(recursive_result, inverse);
+            }
+
+        */
+
+        
         //Get Recursive Result
         //Need to put this into a combined kernel
-        //this.getRecursive.setOutput([len]);
-        //this.getRecursive(input,p);
+        this.getRecursive.setOutput([len]);
+        recursive_result = this.getRecursive(input,p);
 
         if(m > 1){
           recursive_result = this.fft(recursive_result,len,inverse); //This is a scoping issue, need to do this in the combined kernel and have it call itself to not pass things between CPU and GPU at all unless absolutely necessary (only for input and output ideally)
         }
+
+        /*
+        
+            const del_f_r = Math.cos(2*PI*j/n);
+            const del_f_i = (inverse ? -1 : 1) * Math.sin(2*PI*j/n);
+            let f_r = 1;
+            let f_i = 0;
+
+            for(let i = 0; i < n; i++) { //this.thread.y
+              const _real = recursive_result.real[i % m];
+              const _imag = recursive_result.imag[i % m];
+
+              output.real[i] += f_r * _real - f_i * _imag;
+              output.imag[i] += f_r * _imag + f_i * _real;
+
+              [f_r, f_i] = [
+                f_r * del_f_r - f_i * del_f_i,
+                f_i = f_r * del_f_i + f_i * del_f_r,
+              ];
+            }
+          }
+        */
+
 
         //This section should be scoped into its own kernel as it is its own loop
         const del_f_r = Math.cos(2*PI*this.thread.y/len);
@@ -133,6 +223,59 @@ class gpuUtils {
       //WIP
       this.FFT_Iterative = this.gpu.createKernel(function(input, len, inverse){
         var output = new Array(2);
+
+          /*
+          const n = input.length;
+
+          const output = BitReverseComplexArray(input);
+          const output_r = output.real;
+          const output_i = output.imag;
+
+
+          */
+          const n = len;
+
+          const output = BitReverseComplexArray(input);
+          const output_r = output[0];
+          const output_i = output[1];
+
+
+          /*
+          // Loops go like O(n log n):
+          //   width ~ log n; i,j ~ n
+          let width = 1;
+          while (width < n) {
+            const del_f_r = Math.cos(PI/width);
+            const del_f_i = (inverse ? -1 : 1) * Math.sin(PI/width);
+            for (let i = 0; i < n/(2*width); i++) {
+              let f_r = 1;
+              let f_i = 0;
+              for (let j = 0; j < width; j++) {
+                const l_index = 2*i*width + j;
+                const r_index = l_index + width;
+
+                const left_r = output_r[l_index];
+                const left_i = output_i[l_index];
+                const right_r = f_r * output_r[r_index] - f_i * output_i[r_index];
+                const right_i = f_i * output_r[r_index] + f_r * output_i[r_index];
+
+                output_r[l_index] = SQRT1_2 * (left_r + right_r);
+                output_i[l_index] = SQRT1_2 * (left_i + right_i);
+                output_r[r_index] = SQRT1_2 * (left_r - right_r);
+                output_i[r_index] = SQRT1_2 * (left_i - right_i);
+
+                [f_r, f_i] = [
+                  f_r * del_f_r - f_i * del_f_i,
+                  f_r * del_f_i + f_i * del_f_r,
+                ];
+              }
+            }
+            width <<= 1;
+          }
+
+          return output;
+        */
+       return output;
 
 
       }).setOutput([100])
